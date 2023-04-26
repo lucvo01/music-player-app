0ae54a871f51e255e129c9ec2dfdba65b9203466
Luc Vo <luke.syvo@gmail.com>
2023-04-25 21:51:59 -0600

    .

---
+++ src/component/TrackList.js
@@ -0,0 +1,18 @@
+import {useMusicPlayer} from "../contexts/MusicPlayerContext"
+
+const TrackList = () => {
+const { trackList, currentTrackName, playTrack, isPlaying } = useMusicPlayer();
+
+return (
+  <>
+    {trackList.map((track, index) => (
+      // ( Surprise us with your code here)
+
+        <div className="song-title">{track.name}</div>
+ 
+    ))}
+  </>
+);
+};
+
+export default TrackList;
